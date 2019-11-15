7b0e630cca57963b605d4ae8225dcacffc6a12eb
Sven Koehler <sven.koehler@hpi.de>
2019-11-15 11:19:01 +0100

    Added amin site

--- records/models.py
+++ records/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Zone(models.Model):
 	zone = models.CharField(max_length=128, unique=True)
 
+	def __str__(self):
+		return self.zone
+
 
 class RecordName(models.Model):
 	name = models.CharField(max_length=128)
@@ -17,6 +20,9 @@ class RecordName(models.Model):
 	class Meta:
 		unique_together = (("name", "zone"), )
 
+	def __str__(self):
+		return ".".join((self.name, self.zone.zone))
+
 
 class Record(models.Model):
 	rname  = models.ForeignKey(RecordName, 
@@ -27,4 +33,7 @@ class Record(models.Model):
 	rtype  = models.CharField(max_length=16)
 	rttl   = models.PositiveIntegerField(default=300)
 	rdata  = models.CharField(max_length=128)
+
+	def __str__(self):
+		return "Record(%s, %s, %s)" % (self.rname, self.rtype, self.rdata)
 
