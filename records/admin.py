5990480b13f2d4293116be224edfe172e7dc2a98
Sven Koehler <sven.koehler@hpi.de>
2019-11-21 18:04:29 +0100

    Name add: The oldest zone is not the default zone (closes #9)

--- records/admin.py
+++ records/admin.py
@@ -4,6 +4,7 @@ from .models import *
 
 @admin.register(Zone)
 class ZoneAdmin(admin.ModelAdmin):
+
 	def get_readonly_fields(self, request, obj=None):
 		if obj is None:
 			return []
@@ -13,15 +14,25 @@ class ZoneAdmin(admin.ModelAdmin):
 
 @admin.register(RecordName)
 class RecordNameAdmin(admin.ModelAdmin):
+
 	def get_readonly_fields(self, request, obj=None):
 		if obj is None:
 			return []
 		else:
 			return ["zone", "name"]
 
+	def get_form(self, request, obj=None, **kwargs):
+		form = super(RecordNameAdmin, self).get_form(request, obj, **kwargs)
+		if obj is None:
+			form.base_fields['zone'].initial = self.availableZones(request).first()
+		return form
+
+	def availableZones(self, request):
+		return Zone.objects.filter().order_by('pk')
 
 @admin.register(Record)
 class RecordAdmin(admin.ModelAdmin):
+
 	def get_readonly_fields(self, request, obj=None):
 		if obj is None:
 			return []
